75ea95dc979bb4a5003865a2d91d69ff8951833c
iloveuhyeon <pyh1111802@naver.com>
2023-11-09 14:38:24 +0900

    11.09

--- Dart, flutter/practice/main.dart
+++ Dart, flutter/practice/main.dart
@@ -282,3 +282,15 @@
 
 //   print(name);
 // }
+
+typedef typelist = List<int>;
+
+typelist hi(typelist name) {
+  var reverse = name.reversed;
+  print(reverse);
+  return reverse.toList();
+}
+
+void main() {
+  print(hi([1, 2, 3]));
+}
